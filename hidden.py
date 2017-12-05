ae6b509efae268a76d9fc120fb82f489bd402313
wuhuaibi <wuhuaibi@foxmail.com>
2017-12-05 16:48:48 +0800

    twitter

---
+++ hidden.py
@@ -0,0 +1,11 @@
+# Keep this file separate
+
+def oauth() :
+    # return { "consumer_key" : "h7Lu...Ng",
+    #     "consumer_secret" : "dNKenAC3New...mmn7Q",
+    #     "token_key" : "10185562-eibxCp9n2...P4GEQQOSGI",
+    #     "token_secret" : "H0ycCFemmC4wyf1...qoIpBo" }
+    return { "consumer_key" : "I1XowkiqMs94Ac72fEp2CXPv0",
+        "consumer_secret" : "drfnZHVUQrq1dyeqepCrbKyq7HBGWeYJCeTFQZpkLcXkgKFw3P",
+        "token_key" : "	936432882482143235-jNLGPKcasCpZaSqR1D2WarSEshgQcyi",
+        "token_secret" : "YF4ddleSgGxj8BsfmH2eJE50DErFeLr7TsNNKAp08ZvqC" }
